ec022b2af4a28056ce124b64eb239ee8276a0830
JacoboJacome <jacobojacome2@gmail.com>
2022-03-22 17:04:35 -0500

    admin

--- core/models.py
+++ core/models.py
@@ -26,6 +26,7 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
+
     def create_superuser(self, email, password):
         user = self.create_user(
             email, password=password
