8db4740bd97a497b06b4dea1b962baddfc7f0c2e
JacoboJacome <jacobojacome2@gmail.com>
2022-03-22 16:17:53 -0500

    Make_password

--- system_manage/serializers.py
+++ system_manage/serializers.py
@@ -4,12 +4,12 @@ from system_manage.models import UserProfile
 
 User = get_user_model()
 
-class Librian(serializers.ModelSerializer):
+class LibrianUser(serializers.ModelSerializer):
     class Meta:
         model : UserProfile
         fields = ('name', 'last_mame','staff', 'user_name','id')
 
-class Member(serializers.ModelSerializer):
+class MemberUser(serializers.ModelSerializer):
     class Meta:
         model : UserProfile
         fields = ('name', 'last_mame','staff','user_name','date_of_membership','id')
