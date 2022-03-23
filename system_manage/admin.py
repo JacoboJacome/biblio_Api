97295f098243808d3a4a4776be2fabdfb883225a
JacoboJacome <jacobojacome2@gmail.com>
2022-03-22 22:26:53 -0500

    admin

--- system_manage/admin.py
+++ system_manage/admin.py
@@ -1,3 +1,52 @@
-from django.contrib import admin
+from datetime import datetime
 
+from django.contrib import admin
+from .models import *
 # Register your models here.
+
+# admin.site.empty_value_display = "No valor"
+
+class AuthorAdmin(admin.ModelAdmin):
+    empty_value_display = "Unknow"
+    fieldsets = [
+        (None, {'fields': ['name','last_name','description']})
+    ]
+
+    list_display = ('name', 'last_name','description')
+    search_fields = ('name','last_name',)
+
+class BookItemAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Books Information", {
+            'fields': ('title','added', 'subject')
+        }),('The author', {
+            'fields': ('ref_author',),
+        })
+
+    ]
+
+    list_display = ('added','book')
+    list_display_links = ('book',)
+    # list_editable = ('question_text',)
+    # date_hierarchy = 'pub_date'
+    save_on_top = True
+    # list_filter = ('title',)
+    # autocomplete_fields = ['ref_author']
+
+
+    # def has_been_published(self, obj):
+    #     present =  datetime.now()
+    #     return obj.pub_date.date() < present.date()
+
+    # has_been_published.boolean = True
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title','author')
+    list_filter = ('title',)
+    search_fields = ('title',)
+    # list_display_links = ('author',)
+
+
+admin.site.register(Author,AuthorAdmin)
+admin.site.register(BookItem, BookItemAdmin)
+admin.site.register(Book)
--- system_manage/models.py
+++ system_manage/models.py
@@ -5,8 +5,8 @@ from django.conf import settings
 User = settings.AUTH_USER_MODEL
 
 class Author(models.Model):
-    name = models.CharField(max_length=100),
-    last_name = models.CharField(max_length=100),
+    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     description = models.CharField(max_length=255)
 
     def get_name(self):
@@ -20,24 +20,24 @@ class Library(models.Model):
         return self.address
 
 class Book(models.Model):
-    title = models.CharField(max_length=100),
-    subject = models.CharField(max_length=100),
-    added = models.DateField(),
-    published = models.CharField(max_length=50),
-    language = models.CharField(max_length=100),
-    number_of_pages = models.IntegerField(),
+    title = models.CharField(max_length=100)
+    subject = models.CharField(max_length=100)
+    added = models.DateField()
+    published = models.CharField(max_length=50)
+    language = models.CharField(max_length=100)
+    number_of_pages = models.IntegerField()
     author = models.ManyToManyField(Author)
 
     def get_title(self):
         return self.title
 
 class BookItem(models.Model):
-    title = models.CharField(max_length=100),
-    subject = models.CharField(max_length=100),
-    added = models.DateField(),
-    published = models.CharField(max_length=50),
-    language = models.CharField(max_length=100),
-    number_of_pages = models.IntegerField(),
+    title = models.CharField(max_length=100)
+    subject = models.CharField(max_length=100)
+    added = models.DateField()
+    published = models.CharField(max_length=50)
+    language = models.CharField(max_length=100)
+    number_of_pages = models.IntegerField()
     bar_code = models.CharField(max_length=100)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     library = models.ForeignKey(Library, on_delete=models.CASCADE)
