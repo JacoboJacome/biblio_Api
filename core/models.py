1d291bbe0e19e35e8f8c58c02f5ced08e33b8863
JacoboJacome <jacobojacome2@gmail.com>
2022-03-22 20:28:20 -0500

    add create superuser

--- core/models.py
+++ core/models.py
@@ -11,8 +11,6 @@ class UserManager(BaseUserManager):
 
         user = self.model(
             email=self.normalize_email(email),
-            # name=self.normalize_name(name),
-            # last_name=self.normalize_last_name(last_name)
         )
         user.set_password(password)
         user.save(using=self._db)
