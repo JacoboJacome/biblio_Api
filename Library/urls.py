34cb419c6cf4c54074fe3aec0490327b1d31b18b
JacoboJacome <jacobojacome2@gmail.com>
2022-04-06 18:57:13 -0500

    tokens

--- Library/urls.py
+++ Library/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/users/', include('core.urls')),
     path('api/v1/library/', include('system_manage.urls'))
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
